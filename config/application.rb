e1995fe48288f0d40c5413e20cbdbced36fb6ed1
toshichanapp <toshichanapp@gmail.com>
2021-08-11 19:50:02 +0900

    pry,rubocopの追加、localの追加、time_zoneの設定

--- config/application.rb
+++ config/application.rb
@@ -29,12 +29,16 @@ module QuizPlatform
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.test_framework false
+    end
   end
 end
