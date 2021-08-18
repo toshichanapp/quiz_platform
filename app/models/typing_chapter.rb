5e99b71bdc60043746133bd2336d3a2b83f7aa67
toshichanapp <toshichanapp@gmail.com>
2021-08-18 18:00:08 +0900

    fix

--- app/models/typing_chapter.rb
+++ app/models/typing_chapter.rb
@@ -2,7 +2,7 @@
 
 class TypingChapter < ApplicationRecord
   belongs_to :book
-  has_many :typing_questions
+  has_many :typing_questions, dependent: :nullify
 
   validates :key, presence: true, uniqueness: { scope: :book }, format: { with: /\A[0-9a-z]+\z/ }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,33 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_814_065_139) do
+ActiveRecord::Schema.define(version: 2021_08_18_085227) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'pgcrypto'
-  enable_extension 'plpgsql'
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
-  create_table 'books', force: :cascade do |t|
-    t.string 'title'
-    t.string 'key'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "key"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["key"], name: "index_books_on_key", unique: true
   end
 
-  create_table 'typing_chapters', force: :cascade do |t|
-    t.bigint 'book_id'
-    t.string 'title'
-    t.string 'key'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['book_id'], name: 'index_typing_chapters_on_book_id'
+  create_table "typing_chapters", force: :cascade do |t|
+    t.bigint "book_id"
+    t.string "title"
+    t.string "key"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_typing_chapters_on_book_id"
+    t.index ["key", "book_id"], name: "index_typing_chapters_on_key_and_book_id", unique: true
   end
 
-  create_table 'typing_questions', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.bigint 'typing_chapter_id'
-    t.string 'body'
-    t.string 'typing_body'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['typing_chapter_id'], name: 'index_typing_questions_on_typing_chapter_id'
+  create_table "typing_questions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.bigint "typing_chapter_id"
+    t.string "body"
+    t.string "typing_body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["typing_chapter_id"], name: "index_typing_questions_on_typing_chapter_id"
   end
+
 end
